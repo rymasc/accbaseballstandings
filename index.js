26db17f8e5b1727f832307b5af5b90e254f470e8
Ryan Scott <rscott6@clemson.edu>
2018-04-22 16:53:41 -0400

    Add team data to team object

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var cheerio = require('cheerio');
 var request = require('request');
 const fs = require('fs');
-const ws = 'https://d1baseball.com/'
+const ws = 'https://d1baseball.com'
 
 request('https://d1baseball.com/conference/atlantic-coast-conference', function (error, response, html) {
   if (!error && response.statusCode == 200) {
@@ -20,17 +20,31 @@ request('https://d1baseball.com/conference/atlantic-coast-conference', function 
                 switch(i){
                     case 0:
                         team.name = $(this).text();
-                        console.log($(this).find("a").attr('href'));
+                        team.website = ws + $(this).find("a").attr('href');
                         break;
                     case 1:
-                        team.record = $(this).text();
+                        team.conf_record = $(this).text();
                         break;
+                    case 2:
+                        team.conf_perc = $(this).text();
+                        break;
+                    case 3:
+                        gamesback = $(this).text();
+                        team.gamesback = (gamesback == '-') ? 0 : gamesback; //make it zero instead of - for divisional leader.
+                        break
+                    case 4:
+                        team.overall_record = $(this).text();
+                        break;
+                    case 5:
+                        team.overall_perc = $(this).text();
+                        break;
+                    case 6:
+                        team.streak = $(this).text();    
                 }
             });
             teams.push(team);
-
         });
-
+        console.log(teams[0]);
     });
   }
 });
