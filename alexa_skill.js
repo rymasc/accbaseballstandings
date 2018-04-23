356038756c12e7ef2569e8d9fe5cec065267b7d7
Ryan Scott <rscott6@clemson.edu>
2018-04-23 00:08:24 -0400

    Change to alex-sdk API

--- alexa_skill.js
+++ alexa_skill.js
@@ -1,24 +1,50 @@
-exports.handler = (event, context) => {
+const Alexa = require('alexa-sdk');
 
-    if(event.session.new){
-        console.log("New Session");
-    }
+const SKILL_NAME = 'ACC Baseball';
+const GET_FACT_MESSAGE = "Here's your fact: ";
+const HELP_MESSAGE = 'You can say tell me a space fact, or, you can say exit... What can I help you with?';
+const HELP_REPROMPT = 'What can I help you with?';
+const STOP_MESSAGE = 'Goodbye!';
 
-    switch (event.request.type){
 
-        case "LaunchRequest":
-            console.log("LaunchRequest");
-            break;
-        
-        case "IntentRequest":
-            console.log("IntentRequest");
-            break;
+//=========================================================================================================================================
+//Editing anything below this line might break your skill.
+//=========================================================================================================================================
 
-        case "SessionEndedRequest":
-            console.log("SessionEndedRequest");
-            break;
-        
-        default:
-            context.fail(`Invalid request type: ${event.request.type}`);
-    }
-}
+const handlers = {
+    'LaunchRequest': function () {
+        this.response.speak("Hello welcome to " + SKILL_NAME);
+        this.emit('GetNewFactIntent');
+    },
+    // 'GetNewFactIntent': function () {
+    //     const factArr = data;
+    //     const factIndex = Math.floor(Math.random() * factArr.length);
+    //     const randomFact = factArr[factIndex];
+    //     const speechOutput = GET_FACT_MESSAGE + randomFact;
+
+    //     this.response.cardRenderer(SKILL_NAME, randomFact);
+    //     this.response.speak(speechOutput);
+    //     this.emit(':responseReady');
+    // },
+    'AMAZON.HelpIntent': function () {
+        const speechOutput = HELP_MESSAGE;
+        const reprompt = HELP_REPROMPT;
+
+        this.response.speak(speechOutput).listen(reprompt);
+        this.emit(':responseReady');
+    },
+    'AMAZON.CancelIntent': function () {
+        this.response.speak(STOP_MESSAGE);
+        this.emit(':responseReady');
+    },
+    'AMAZON.StopIntent': function () {
+        this.response.speak(STOP_MESSAGE);
+        this.emit(':responseReady');
+    },
+};
+
+exports.handler = function (event, context, callback) {
+    const alexa = Alexa.handler(event, context, callback);
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
